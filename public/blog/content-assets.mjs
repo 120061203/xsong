e27eae0df5834bb3aead2e9f8eddc2f12fac78bf
songlin.chen <songlin.chen@pacston.com>
2025-09-06 20:50:38 +0800

    Add my-first-article image and update heroImage

--- public/blog/content-assets.mjs
+++ public/blog/content-assets.mjs
@@ -1,4 +1,5 @@
 
 import __ASTRO_IMAGE_IMPORT_1jGFRv from "../../assets/images/work-one-month-reflection/work-one-month-reflection-1.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fwork-one-month-reflection.md";
-export default new Map([["../../assets/images/work-one-month-reflection/work-one-month-reflection-1.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fwork-one-month-reflection.md", __ASTRO_IMAGE_IMPORT_1jGFRv]]);
+import __ASTRO_IMAGE_IMPORT_Z2gzuJA from "../../assets/images/my-first-article/my-first-article-1.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fmy-first-article.md";
+export default new Map([["../../assets/images/work-one-month-reflection/work-one-month-reflection-1.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fwork-one-month-reflection.md", __ASTRO_IMAGE_IMPORT_1jGFRv], ["../../assets/images/my-first-article/my-first-article-1.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fmy-first-article.md", __ASTRO_IMAGE_IMPORT_Z2gzuJA]]);
 		
