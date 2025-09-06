e0d0cb10cfe0fb938bb0ceadd0ecbcaf24954085
songlin.chen <songlin.chen@pacston.com>
2025-09-06 19:16:44 +0800

    Add work reflection image for blog heroImage

--- public/blog/content-assets.mjs
+++ public/blog/content-assets.mjs
@@ -1,4 +1,1 @@
-
-import __ASTRO_IMAGE_IMPORT_2fy1vy from "./work-one-month-reflection-1.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fwork-one-month-reflection.md";
-export default new Map([["./work-one-month-reflection-1.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fwork-one-month-reflection.md", __ASTRO_IMAGE_IMPORT_2fy1vy]]);
-		
+export default new Map();
