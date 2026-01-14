1aa52263b7d048b894894f879182e28005932087
songlin.chen <songlin.chen@pacston.com>
2026-01-14 16:28:55 +0800

    fix: 修復 GTM 代碼的 TypeScript 類型錯誤並使用 Next.js Script 組件

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -80,20 +81,6 @@ export default function RootLayout({
   return (
     <html lang="zh-TW" translate="no">
       <head>
-        {/* Google Tag Manager */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-56MXC3D7');
-            `,
-          }}
-        />
-        {/* End Google Tag Manager */}
-        
         {/* 預載入關鍵資源 */}
         <link rel="preload" href="/avatar.png" as="image" />
         <link rel="dns-prefetch" href="//cdnjs.cloudflare.com" />
@@ -128,9 +115,31 @@ export default function RootLayout({
         </noscript>
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-white`} translate="no">
+        {/* Google Tag Manager */}
+        <Script
+          id="google-tag-manager"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-56MXC3D7');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+        
         {/* Google Tag Manager (noscript) */}
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-56MXC3D7"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-56MXC3D7"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         {/* End Google Tag Manager (noscript) */}
         
         {/* Google Analytics */}
